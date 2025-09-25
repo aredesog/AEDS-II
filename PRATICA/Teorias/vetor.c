ca899b0c4db11264394837dcf5def3c753fe7561
Guilherme Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2025-09-25 19:15:37 -0300

    Implementa função para remoção de número em vetor e exibe vetor atualizado

---
+++ PRATICA/Teorias/vetor.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct proximo{
+    int valor;
+};
+
+int remove_numero(int L[]){
+    int numero;
+    
+    printf("Qual numero quer remover? ");
+    scanf("%d", &numero);
+    
+    // Procura o número no vetor
+    for (int i = 0; i < 10; i++){
+        if(numero == L[i]){
+            printf("Numero %d encontrado na posicao: %d\n", numero, i);
+            
+            // Move todos os elementos à esquerda
+            for(int j = i; j < 9; j++){
+                L[j] = L[j + 1];
+            }
+            
+            // Coloca 0 na última posição
+            L[9] = 0;
+
+            //tira uma casa do vetor
+            
+            return 1; // Indica que removeu com sucesso
+        }
+    }
+    
+    printf("Numero %d nao encontrado no vetor!\n", numero);
+    return 0; // Indica que não encontrou
+}
+
+int main(){
+    int L[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    printf("Vetor original: ");
+    for(int i = 0; i < 10; i++){
+        printf("%d ", L[i]);
+    }
+    printf("\n");
+
+    remove_numero(L);
+
+    printf("Vetor apos remocao: ");
+    for(int i = 0; i < 10; i++){
+        printf("%d ", L[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
