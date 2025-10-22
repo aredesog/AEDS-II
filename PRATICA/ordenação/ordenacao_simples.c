1e758fca3d8ec87ff4277c5e03db7837b3589055
Guilherme <guilherme.aredes@sou.unifal-mg.edu.br>
2025-10-22 10:58:54 -0300

    Adiciona implementação da função de ordenação por inserção e ajusta a impressão do vetor

--- PRATICA/ordenação/ordenacao_simples.c
+++ PRATICA/ordenação/ordenacao_simples.c
@@ -62,8 +62,21 @@ void selection(int *vetor, int n) {
 void insertion(int *vetor) {
     
     imprimir(vetor);
+    printf("\n");
 
-    
+    for (int i = 1; i < 50; i++) {
+        int key = vetor[i];
+        int j = i - 1;
+
+        while (j >= 0 && vetor[j] > key) {
+            vetor[j + 1] = vetor[j];
+            j--;
+        }
+        vetor[j + 1] = key;
+
+        imprimir(vetor);
+    }
+
     return;
 }
 
